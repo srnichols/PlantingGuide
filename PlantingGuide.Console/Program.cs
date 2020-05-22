e52b62f39b4c73d9693a9f4d4faaf57e4a45cf4c
Scott Nichols <srnichols@live.com>
2020-05-22 13:27:55 -0600

    removed console

---
+++ PlantingGuide.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace PlantingGuide.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
